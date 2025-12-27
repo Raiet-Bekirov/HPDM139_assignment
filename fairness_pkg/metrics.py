2433ef551f36f5811b44eb0c6d74463a1a2910e4
Becky Griffiths <reg222@exeter.ac.uk>
2025-12-27 16:54:21 +0000

    Restructure package code

--- fairness_pkg/metrics.py
+++ fairness_pkg/metrics.py
@@ -1,2 +1,32 @@
-def test_func(a,b):
-    print(a**b)
+import numpy as np
+from itertools import product
+
+def accuracy(a,b):
+    print(a ** b)
+    
+def accuracy_diff():
+	pass
+
+def accuracy_ratio():
+	pass
+	
+def intersect_accuracy_diff():
+	pass
+	
+def intersect_accuracy_ratio():
+	pass
+    
+def false_positive_rate():
+    pass
+    
+def false_negative_rate():
+    pass
+    
+def positive_predictive_value():
+    pass
+    
+def negative_predictive_value():
+    pass
+    
+def negative_predictive_value():
+    pass

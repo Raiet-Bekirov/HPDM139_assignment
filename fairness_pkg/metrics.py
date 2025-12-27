2e883532410806824ada794b0b6a2af5c72cb36c
Becky Griffiths <reg222@exeter.ac.uk>
2025-12-27 17:42:05 +0000

    Start work on fairness functions

--- fairness_pkg/metrics.py
+++ fairness_pkg/metrics.py
@@ -1,32 +1,63 @@
 import numpy as np
 from itertools import product
 
-def accuracy(a,b):
-    print(a ** b)
+
+def group_accuracy(group_label, subject_labels,
+                   predictions, true_statuses):
+    n_samples = len(predictions)
+    accurate_or_not = [pred == truth \
+                       for pred, truth in
+                       zip(predictions, true_statuses)]
+    mask = [True] * n_samples
+    for observation in range(n_samples):
+        if subject_labels[observation] != group_label:
+            mask[observation] = False
+    group_results = [acc for acc, include in \
+                     zip(accurate_or_not, mask) \
+                     if include == True]
+    if len(group_results) > 0:
+        accuracy = sum(group_results) / len(group_results)
+    else:
+        accuracy = np.nan
+    return(accuracy)
     
-def accuracy_diff():
-	pass
+
+def accuracy_diff(group_a_label, group_b_label, subject_labels,
+                  predictions, true_statuses):
+    pass
+
 
 def accuracy_ratio():
-	pass
-	
+    pass
+    
+
+def accuracy_ratio_logged():
+    pass
+    
+
 def intersect_accuracy_diff():
-	pass
-	
+    pass
+    
+
 def intersect_accuracy_ratio():
-	pass
-    
-def false_positive_rate():
     pass
     
-def false_negative_rate():
+
+def intersect_accuracy_ratio_logged():
     pass
     
-def positive_predictive_value():
+
+def group_fnr():
     pass
     
-def negative_predictive_value():
+
+def group_fpr():
     pass
     
-def negative_predictive_value():
+
+def group_ppv():
+    pass
+    
+
+def group_npv():
     pass
